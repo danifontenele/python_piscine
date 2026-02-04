3432280932424dc1978cf879724d8f5c86230b46
calvares <calvares@student.42lisboa.com>
2026-02-04 16:39:12 +0000

    security improved

--- modulo_01_python/ex04/ft_garden_security.py
+++ modulo_01_python/ex04/ft_garden_security.py
@@ -1,9 +1,9 @@
 class SecurePlant:
     def __init__(self, name: str, height: int, age: int):
-        self.name = name
-        self.height = height
-        self.age = age
-        print(f"Plant created: {self.name}\n")
+        self.__name = name
+        self.__height = height
+        self.__age = age
+        print(f"Plant created: {self.__name}\n")
 
     def set_height(self, new_height: int):
         if new_height < 0:
@@ -11,8 +11,8 @@ class SecurePlant:
                   f"{new_height}cm [REJECTED]")
             print("Security: Negative height rejected\n")
         else:
-            self.height = new_height
-            print(f"Height updated: {self.height}cm [OK]")
+            self.__height = new_height
+            print(f"Height updated: {self.__height}cm [OK]")
 
     def set_age(self, new_age: int):
         if new_age < 0:
@@ -20,14 +20,17 @@ class SecurePlant:
                   f"{new_age} days old [REJECTED]")
             print("Security: Negative age rejected\n")
         else:
-            self.age = new_age
-            print(f"Age updated: {self.age} days [OK]")
+            self.__age = new_age
+            print(f"Age updated: {self.__age} days [OK]")
+
+    def get_name(self):
+        return self.__name
 
     def get_height(self):
-        return self.height
+        return self.__height
 
     def get_age(self):
-        return self.age
+        return self.__age
 
 
 if __name__ == "__main__":
@@ -35,6 +38,7 @@ if __name__ == "__main__":
     rose.set_height(25)
     rose.set_age(30)
     rose.set_age(-5)
-    height = rose.get_height()
-    age = rose.get_age()
-    print(f"Current plant: {rose.name} ({height}cm, {age} days)")
+    rose_name = rose.get_name()
+    rose_height = rose.get_height()
+    rose_age = rose.get_age()
+    print(f"Current plant: {rose_name} ({rose_height}cm, {rose_age} days)")
