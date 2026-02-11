7d7cd1fdd70c961b785a516398ef00d8a622d8eb
carlos alvares <calvares@student.42lisboa.com>
2026-02-11 16:58:06 +0000

    understanding try and except

---
+++ modulo_02_python/ex01/ft_different_errors.py
@@ -0,0 +1,19 @@
+def garden_operations(file: str) -> int | None:
+	try:
+		#abrir ficheiro
+		#ler conteudo
+		#converter valores
+		#fazer calculo
+		#acessar chave
+	except ValueError:
+		#tratar
+	except ZeroDivisionError:
+		#tratar
+	except FileNotFoundError:
+		#tratar
+	except KeyError:
+		#tratar
+	finally:
+		#fechar ficheiro
+
+if __name__ == "__main__":
